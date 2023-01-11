d4fbfef1913907ea69704fee41ce268571d98b3b
Ozan <114956724+DevriN34@users.noreply.github.com>
2023-01-11 18:04:59 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+        foreach (var Product in productManager.GetAll())
+        {
+            Console.WriteLine(Product.ProductName);
+        }
+    }
+}
